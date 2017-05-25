065a6aa824b679d198feb0201a6ad7efc9caa547
Michael Cornillon <contact@dwaps.fr>
2017-05-25 11:24:58 +0200

    Débug : slideshow fonctionnel toutes pages

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,7 +2,11 @@ const NB_SLIDES = 8;
 const SPEED = 2000; // en ms
 
 var slideshow = document.querySelector("#slideshow");
+var script = document.querySelector("script");
+var pathSlide = script.dataset.nohome == "true" ? "../img/slideshow/slide" : "img/slideshow/slide";
 var cpt = 0; // compteur de slides
+
+console.log("Chemin vers les slides : ", pathSlide);
 
 
 // FONCTIONS
@@ -15,7 +19,7 @@ function startSlideshow()
       cpt++;
 
       // On change l'image du slideshow grâce au compteur de slides
-      slideshow.style.backgroundImage = "url('img/slideshow/slide"+cpt+".jpg')";
+      slideshow.style.backgroundImage = "url('"+pathSlide+cpt+".jpg')";
 
       // On réinitialise le compteur de slides
       // si on a atteint la dernière image
